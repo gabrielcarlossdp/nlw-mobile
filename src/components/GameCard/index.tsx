ad9c9ccfd3a75b39d53065d71f80072905a2a471
gabrielcarlossdp <gabriel_webdesigner@hotmail.com>
2022-09-17 14:40:49 -0300

    connect api

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -8,10 +8,10 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { THEME } from '../../theme';
 
 export interface GameCardProps {
-    id: String;
-    name: String;
-    ads: String;
-    cover: ImageSourcePropType;
+    id: string;
+    title: string;
+    _count:{ads: number};
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -23,17 +23,17 @@ export function GameCard({ data, ...rest }: Props) {
         <TouchableOpacity style={styles.container} {...rest}>
             <ImageBackground
                 style={styles.cover}
-                source={data.cover}
+                source={{uri:data.bannerUrl}}
             >
                 <LinearGradient
                     colors={THEME.COLORS.FOOTER}
                     style={styles.footer}>
 
                     <Text style={styles.name}>
-                        {data.name}
+                        {data.title}
                     </Text>
                     <Text style={styles.ads}>
-                        {data.ads} anúncios
+                        {data._count.ads} anúncios
                     </Text>
 
                 </LinearGradient>
